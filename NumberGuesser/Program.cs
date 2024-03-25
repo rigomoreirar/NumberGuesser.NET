5e0dca90ee1bd0988495147d5f3a39fccd2989bb
rigomoreirar <rigomoreirar@gmail.com>
2024-03-25 15:32:54 -0600

    Add project files.

---
+++ NumberGuesser/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+//using System.Runtime.CompilerServices;
+//using system.collections.generic;
+//using system.linq;
+//using system.text;
+//using system.threading.tasks;
+
+
+// Namespace
+namespace NumberGuesser
+{
+
+    //Main Class
+    internal class Program
+    {
+
+        
+
+
+        // Entry Point Method
+        static void Main(string[] args)
+        {
+            
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Brad Traversy";
+
+
+            // Change text color
+            //Console.
+
+            Console.WriteLine("{0}: version {1} by {2}", appName, appVersion, appAuthor);
+        }
+    }
+}
